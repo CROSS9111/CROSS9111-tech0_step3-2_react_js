e8eb6d5e58706fbddbca16c212b2acb6a0d2b1a6
H.Kurosu <kurosuhirotoshi@kurosuhirotoshinoMacBook-Air.local>
2023-11-08 15:13:48 +0900

    comment add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import ImageGrallery from './ImageGrallery';
 import { useRef, useState } from 'react';
 
 function App() {
+  // useState : 関数コンポーネント
+  // 状態管理のためのフック。コンポーネントのレンダー間で値を保持し、その値を更新する。更新した時に再レンダリングする。
   const [fetchData,setFetchdata] = useState([]);
+  // useRef : 関数コンポーネント
+  // 参照（ref）を管理するためのフック。useRefを更新しても再レンダリングはされない。
   const ref = useRef(); // refに入力した値を取得する
 
   const handleSubmit = (e) => {
@@ -12,10 +16,14 @@ function App() {
     console.log(ref.current.value);
     
     //APIURL
-    //バッククォーテーション　テンプレートリテラル
-    const endpointURL = `https://pixabay.com/api/?key=XXXXXXXXXXXXXXXXX=${ref.current.value}&image_type=photo`;
+    //テンプレートリテラルとして、Refの値で変更。
+    //クエリパラメータとして入れている。
+    //APIの中身の説明する
+    
+    const endpointURL = `https://pixabay.com/api/?key=xxxxxxxxxxxxxxxxxxxxx=${ref.current.value}&image_type=photo`;
 
     //apiを叩く（データフェッチング）
+    //python:getと同じ
     fetch(endpointURL).then((res) => {
       return res.json();
       })
@@ -25,14 +33,17 @@ function App() {
         setFetchdata(data.hits); 
       });
   };
-
+// HTMLの中身の説明を挟む
   return (
     <div className="container">
       <h2>My pix</h2>
-      {/* 提出した際にrefを取得 */}
+      {/* 送信した際にrefを取得 */}
       <form onSubmit={(e) => handleSubmit(e)}>
+        {/* inputに入力した情報をuseRefに入れる */}
         <input type="text" placeholder='画像を探す' ref={ref}/>
       </form>
+      {/* ImageGalleryというReactコンポーネントをレンダリングしている
+      fetchDataにあるデータを`ImageGallery`に渡している。 */}
       <ImageGrallery fetchData={fetchData}/>
 
     </div>
--- src/ImageGrallery.js
+++ src/ImageGrallery.js
@@ -5,6 +5,8 @@ const ImageGrallery = ({ fetchData }) => {
   return (
     <div>
         <div className='images-wrapper'>
+            {/* fetchDataにある配列をmapメソッドで処理する。
+            mapを用いることで、各配列を一度ずつdataにオブジェクトとして引き渡し、div要素を実行する。 */}
             {fetchData.map((data) => (
                 <div className='image' key={data.id}>
                     <a href={data.pageURL} target='_blank'>
