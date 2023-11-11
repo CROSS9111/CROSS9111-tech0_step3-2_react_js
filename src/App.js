7c618fa1dbeff5856fc8b44a8a226aa7ec38d443
CROSS9111 <hirotoshikurosu@gmail.com>
2023-11-11 13:56:11 +0900

    readme update

--- src/App.js
+++ src/App.js
@@ -3,47 +3,48 @@ import './App.css';
 import ImageGrallery from './ImageGrallery';
 import { useRef, useState } from 'react';
 
+// React Hooksについて:
+// Hooksは、Reactの関数コンポーネントで状態やライフサイクルなどのReactの機能を使うための関数です。
+// useStateやuseRefなどの標準Hooksがあり、これらを使って様々な機能を実装できます。
+
 function App() {
-  // useState : 関数コンポーネント
-  // 状態管理のためのフック。コンポーネントのレンダー間で値を保持し、その値を更新する。更新した時に再レンダリングする。
+  // useStateフック: コンポーネントの状態（state）を管理するためのフック。
+  // この場合、fetchDataはAPIから取得したデータを保持し、setFetchDataはそのデータを更新する関数。
   const [fetchData,setFetchdata] = useState([]);
-  // useRef : 関数コンポーネント
-  // 参照（ref）を管理するためのフック。useRefを更新しても再レンダリングはされない。
+
+  // useRefフック: コンポーネントで参照を保持するためのフック。
+  // この例では、テキスト入力フィールドへの参照を保持している。
   const ref = useRef(); // refに入力した値を取得する
 
+  // handleSubmit関数: フォーム送信時に実行される。
   const handleSubmit = (e) => {
-    e.preventDefault(); //リロード防止
-    console.log(ref.current.value);
+    e.preventDefault(); // フォーム送信によるページリロードを防ぐ
+    console.log(ref.current.value); // 現在のテキスト入力値をログに出力
     
-    //APIURL
-    //テンプレートリテラルとして、Refの値で変更。
-    //クエリパラメータとして入れている。
-    //APIの中身の説明する
-    
+    // Pixabay APIへのリクエストURLを組み立てる
     const endpointURL = `https://pixabay.com/api/?key=xxxxxxxxxxxxxxxxxxxxx=${ref.current.value}&image_type=photo`;
 
-    //apiを叩く（データフェッチング）
-    //python:getと同じ
+    // Fetch APIを使用してデータを非同期で取得
     fetch(endpointURL).then((res) => {
       return res.json();
       })
       .then((data) => {
-        // {}->オブジェクト　今は配列を定義しているので配列が使えるhitsを指定する。
-        console.log(data.hits);
-        setFetchdata(data.hits); 
+        // data内のhitsを指定する。
+        console.log(data.hits); // 取得したデータのログ出力
+        setFetchdata(data.hits);  // 状態を更新し、コンポーネントを再レンダリング
       });
   };
-// HTMLの中身の説明を挟む
+
   return (
     <div className="container">
       <h2>My pix</h2>
-      {/* 送信した際にrefを取得 */}
+      {/* フォーム送信時にhandleSubmit関数が呼ばれる */}
       <form onSubmit={(e) => handleSubmit(e)}>
-        {/* inputに入力した情報をuseRefに入れる */}
+        {/* テキスト入力フィールド。入力値はuseRefで参照される */}
         <input type="text" placeholder='画像を探す' ref={ref}/>
       </form>
-      {/* ImageGalleryというReactコンポーネントをレンダリングしている
-      fetchDataにあるデータを`ImageGallery`に渡している。 */}
+      {/* ImageGalleryンポーネント)のレンダリング */}
+      {/* fetchDataを`ImageGallery`に渡している。 */}
       <ImageGrallery fetchData={fetchData}/>
 
     </div>
--- src/ImageGrallery.js
+++ src/ImageGrallery.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import "./imageGrallery.css"
 
+// Propsについて:
+// Propsは、Reactコンポーネント間でデータを渡すための仕組みです。
+// 親コンポーネントから子コンポーネントへ値や関数を渡す際に使用され、
+// 子コンポーネントではこれらの値を使用して表示や挙動を制御します。
+
+// ImageGralleryコンポーネント: 外部から受け取った画像データ（fetchData）を表示するためのコンポーネント。
+// ({ fetchData }) と書くことで参照することができる。
 const ImageGrallery = ({ fetchData }) => {
   return (
     <div>
+        {/* 画像を格納するためのコンテナー */}
         <div className='images-wrapper'>
             {/* fetchDataにある配列をmapメソッドで処理する。
             mapを用いることで、各配列を一度ずつdataにオブジェクトとして引き渡し、div要素を実行する。 */}
             {fetchData.map((data) => (
                 <div className='image' key={data.id}>
+                    {/* 各画像を囲むdiv要素。キーはユニークな値であるdata.idを使用。 */}
                     <a href={data.pageURL} target='_blank'>
+                        {/* a要素でラップされたimg要素。クリックすると画像の元のページに遷移する。 */}
                         <img src={data.largeImageURL} 
                             alt=''/>
                     </a>
